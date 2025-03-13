784b8750b3a194644d447e3a3a886e40f0930890
Kwazzi724 <dannyli724@gmail.com>
2025-03-14 02:27:14 +1100

    now smoother player movement, added rotate towards mouse funtion (for back up)

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody rb;
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float acceleration = 8f;
+    [SerializeField] private Camera gameCamera;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 
     void FixedUpdate()
@@ -15,10 +20,34 @@ public class PlayerMovement : MonoBehaviour
         Vector2 inputVector = PlayerInputHandler.instance.moveInput;
         Vector3 moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
         
-        // Quaternion moveRotation = Quaternion.LookRotation(moveDirection);
-        // rb.MovePosition(rb.position + moveDirection * moveSpeed * Time.fixedDeltaTime);
-        Quaternion cameraRotation = Quaternion.Euler(0, 45, 0);
+        // rotate relative to camera
+        Quaternion cameraRotation = Quaternion.Euler(0, 0, 0);
         moveDirection = cameraRotation * moveDirection;
-        rb.linearVelocity = moveDirection * moveSpeed;
+
+        Vector3 targetVelocity = moveDirection * moveSpeed;
+        // realistic movement
+        rb.linearVelocity = Vector3.Lerp(rb.linearVelocity, targetVelocity, acceleration * Time.fixedDeltaTime);
+        
+        // player to face direction of his movement actually
+        if (moveDirection.magnitude > 0.1)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 8f);
+        }
     }
+    
+    /// <summary>
+    /// If we want player to face the direction of mouse
+    /// </summary>
+    // void RotateTowardsMouse()
+    // {
+    //     // raycast from camera into the world to detect mouse position, storing the hit points as Vector3
+    //     Ray ray = gameCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+    //     if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity))
+    //     {
+    //         Vector3 lookAtPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+    //         // player face mouse direction
+    //         transform.LookAt(lookAtPosition);
+    //     }
+    // }
 }
