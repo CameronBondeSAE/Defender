84f834728ff9d9c725dc2210a94c8807c66149df
Kwazzi724 <dannyli724@gmail.com>
2025-03-25 19:53:53 +1100

    cleaning up this branch

--- Assets/Scripts/Ammos/BulletData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Bullet", menuName = "Ammos/BulletData")]
-public class BulletData : AmmoData
-{
-    public float speed;
-    public GameObject bulletPrefab;
-}
