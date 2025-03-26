c547ad639cbe12ea6e22fcc96d1ed6647e4eebd4
Jazz-Owlz <167410150+Jazz-Owlz@users.noreply.github.com>
2025-03-26 12:07:49 +1100

    Starting out Mine
    just the beginning of the mine script.

---
+++ Assets/Scripts/JC Scripts/Mine_Script.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Mine_Script : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+// hows this mine gonna work? 
