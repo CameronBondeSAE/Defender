f23806032008da633f1024341e74c1b27d635282
CameronBondeSAE <30306370+CameronBondeSAE@users.noreply.github.com>
2025-03-26 12:21:24 +1100

    Merge health + Tute stuff

---
+++ Assets/Tutorials/CamPlayer_Controller.cs
@@ -0,0 +1,53 @@
+using System;
+using Tutorials;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CamPlayer_Controller : MonoBehaviour
+{
+	MainControls mainControls;
+
+	public Vector3 direction;
+	public CamPlayer_Model model;
+
+	private void Awake()
+	{
+		mainControls = new MainControls();
+		mainControls.Game.Enable();
+	}
+
+	void OnEnable()
+    {
+	    mainControls.Game.Jump.performed += OnJump;
+	    mainControls.Game.Move.performed += OnMove;
+	    mainControls.Game.Move.canceled += OnMove;
+    }
+
+	private void OnDisable()
+	{
+		mainControls.Game.Jump.performed -= OnJump;
+		mainControls.Game.Move.performed -= OnMove;
+		mainControls.Game.Move.canceled -= OnMove;
+	}
+
+	private void OnMove(InputAction.CallbackContext obj)
+	{
+		// Because this game is on the X,Z plane, not 2D X,Y
+		direction.x = obj.ReadValue<Vector2>().x;
+		direction.z = obj.ReadValue<Vector2>().y;
+		
+		if (obj.performed)
+		{
+			Debug.Log("OnMove = "+direction.x+", "+direction.y);
+		}
+		if(obj.canceled)
+			Debug.Log("OnMove cancelled");
+	}
+
+	private void OnJump(InputAction.CallbackContext obj)
+	{
+		Debug.Log("OnJump");
+		model.Jump();
+	}
+}
