f8c1270ae42773748b38443bed40901fbd28526e
Jazz-Owlz <167410150+Jazz-Owlz@users.noreply.github.com>
2025-04-02 16:22:07 +1100

    Merged into Mine branch

--- Assets/Scripts/JC Scripts/Landmine.cs
+++ Assets/Scripts/JC Scripts/Landmine.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class Landmine : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
+    public bool BigSteps = false; // bool activates a 2-second timer for the landmine to explode, activates  
+    
     // Update is called once per frame
     void Update()
     {
         
     }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.GetComponent<Health>())
+        {
+            Debug.Log("Kaboom"); // in polishing, we could add a UI which prints this // 
+            other.gameObject.GetComponent<Health>().TakeDamage(100);
+            Destroy(other.gameObject);
+        }
+    }
 }
+
+/// Conciderations
+/// Bool can be removed to make mine explosions instant - this may work better in the overall polishing process
+/// otherwise what we can do is increase the timer to 3 seconds and keep the blast radius the same 
