c2a6c5ffed56e216d9f3dabb549332453ca4de4a
Diego <1034055@student.sae.edu.au>
2025-04-26 17:15:45 +1000

    Changed code in Moveablemothership script and base script, making two different types of motherships

--- Assets/Scripts/MothershipBase.cs
+++ Assets/Scripts/MothershipBase.cs
@@ -7,8 +7,6 @@ public class MothershipBase : MonoBehaviour
     [SerializeField] protected GameObject alienPrefab;
     [SerializeField] protected GameObject blueBeam;
     
-    [SerializeField] protected GameObject[] waypoints;
-    
     [SerializeField] protected int alienSpawnCount; //number of aliens that spawn at a time
     [SerializeField] protected float spawnDelay; //the time in seconds it takes to spawn aliens again
 
@@ -16,29 +14,23 @@ public class MothershipBase : MonoBehaviour
     //[SerializeField] protected float spawnTimer;
     [SerializeField] protected bool isSpawningAliens;
 
-    [SerializeField] protected float moveSpeed;
-
     [SerializeField] protected float raycastLength;
     [SerializeField] protected LayerMask raycastPhysicsLayerMasks;
 
     private Vector3 alienSpawnPosition; //The position where the alien will spawn on the map
+    [SerializeField] protected Vector3 alienSpawnOffset;
     
-    void Start()
+    protected virtual void Start()
     {
         //StartCoroutine(SpawnTimer());
         isSpawningAliens = false;
     }
 
-    void Update()
+    protected virtual void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
             SpawnAliens();
-        }
-
-        if (isSpawningAliens == false)
-        {
-            MoveToAWaypoint();
         }
     }
 
@@ -53,9 +45,9 @@ public class MothershipBase : MonoBehaviour
 
     protected virtual IEnumerator SpawnTimer()
     {
+        Debug.Log("test");
         isSpawningAliens = true;
-        yield return StartCoroutine(MoveToAWaypoint());
-        
+ 
         RaycastHit hit;
         if (Physics.Raycast(transform.position, new Vector3(0, -1, 0), out hit, raycastLength,
                 raycastPhysicsLayerMasks))
@@ -66,7 +58,7 @@ public class MothershipBase : MonoBehaviour
         for (int i = 0; i < alienSpawnCount; i++)
         {
             blueBeam.SetActive(true);
-            Instantiate(alienPrefab, alienSpawnPosition + new Vector3(0,1,0), Quaternion.identity);
+            Instantiate(alienPrefab, alienSpawnPosition + alienSpawnOffset, Quaternion.identity);
             yield return new WaitForSeconds(blueBeamDuration);
             blueBeam.SetActive(false);
             yield return new WaitForSeconds(spawnDelay);
@@ -74,18 +66,13 @@ public class MothershipBase : MonoBehaviour
         }
         isSpawningAliens = false;
     }
-
-    protected virtual IEnumerator MoveToAWaypoint()
-    {
-        int randomIndex = Random.Range(0, waypoints.Length - 1);
-        Debug.Log(randomIndex);
-        while ((waypoints[randomIndex].transform.position - transform.position).magnitude > 0.01f)
-        {
-            transform.position = Vector3.Lerp(transform.position, waypoints[randomIndex].transform.position, moveSpeed * Time.deltaTime);
-            yield return null;
-        }
-        transform.position = waypoints[randomIndex].transform.position;
-        
-    }
     
+    /// <summary>
+    /// function to be used by GameManager script to activate waves.
+    /// </summary>
+    public void StartWave()
+    {
+        SpawnAliens();
+    }
+
 }
--- Assets/Scripts/MoveableMothership.cs
+++ Assets/Scripts/MoveableMothership.cs
@@ -1,22 +1,45 @@
+using System.Collections;
 using UnityEngine;
 
 public class MoveableMothership : MothershipBase
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] protected float moveSpeed;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] protected GameObject[] waypoints;
+
+    protected override void Update()
     {
-        
+        base.Update();
+
+        if (isSpawningAliens == false)
+        {
+            MoveToAWaypoint();
+        }
     }
 
     protected override void SpawnAliens()
     {
-        
+        base.SpawnAliens();
+    }
+
+    protected override IEnumerator SpawnTimer()
+    {
+        yield return StartCoroutine(MoveToAWaypoint());
+
+        yield return StartCoroutine(base.SpawnTimer());
+    }
+
+    protected IEnumerator MoveToAWaypoint()
+    {
+        int randomIndex = Random.Range(0, waypoints.Length - 1);
+        Debug.Log(randomIndex);
+        while ((waypoints[randomIndex].transform.position - transform.position).magnitude > 0.01f)
+        {
+            transform.position = Vector3.Lerp(transform.position, waypoints[randomIndex].transform.position, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = waypoints[randomIndex].transform.position;
+
     }
 
 }
