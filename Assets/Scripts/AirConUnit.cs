fde81433df72df552da79929b5012e9aa8ae2f84
Diego <1034055@student.sae.edu.au>
2025-04-28 04:41:08 +1000

    Air con unit can now swing along the y-axis

--- Assets/Scripts/AirConUnit.cs
+++ Assets/Scripts/AirConUnit.cs
@@ -2,7 +2,15 @@ using UnityEngine;
 
 public class AirConUnit : MonoBehaviour
 {
-    [SerializeField] private float pushForce = 10f;
+    [SerializeField] private float pushForce;
+
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float angleRange; 
+
+    private void FixedUpdate()
+    {
+        Swing();
+    }
 
     private void OnTriggerStay(Collider other)
     {
@@ -12,6 +20,15 @@ public class AirConUnit : MonoBehaviour
             Vector3 pushDirection = transform.forward;
             rb.AddForce(pushDirection * pushForce, ForceMode.Force);
          }
+    }
+
+    /// <summary>
+    /// makes aircon rotate side to side
+    /// </summary>
+    private void Swing()
+    {
+        float angleOffset = Mathf.Sin(Time.time * rotationSpeed) * (angleRange / 2f); //range is halfed so it rotates x degrees to each side that adds up to the max range
+        transform.rotation = Quaternion.Euler(0, angleOffset, 0);
     }
 
 }
