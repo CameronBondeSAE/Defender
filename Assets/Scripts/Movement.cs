79acd65a1b79b1d175de36781aad33d0d35da9ec
SAE1\1034055 <1034055@student.sae.edu.au>
2025-04-30 11:09:46 +1000

    Created wave system

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,37 +1,42 @@
 using UnityEngine;
 using System.Collections;
-/// <summary>
-/// Movement used for the mothership
-/// </summary>
 
-public class Movement : MonoBehaviour
+namespace mothershipScripts
 {
-    [SerializeField] private float moveSpeed;
+    /// <summary>
+    /// Movement used for the mothership
+    /// </summary>
 
-    [SerializeField] protected AudioClip[] engineSounds;
-    private AudioSource audioSource;
-
-    void Start()
+    public class Movement : MonoBehaviour
     {
-        audioSource = transform.GetComponent<AudioSource>();    
-    }
+        [SerializeField] private float moveSpeed;
 
-    public IEnumerator MoveToADestination(Vector3 destination)
-    {
-        PlayRandomEngineSound();
-        while ((destination - transform.position).magnitude > 0.01f)
+        [SerializeField] protected AudioClip[] engineSounds;
+        private AudioSource audioSource;
+
+        void Start()
         {
-            transform.position = Vector3.Lerp(transform.position, destination, moveSpeed * Time.deltaTime);
-            yield return null;
+            audioSource = transform.GetComponent<AudioSource>();
         }
-        transform.position = destination; //Snap position destination position
-        audioSource.Stop();
-    }
 
-    protected void PlayRandomEngineSound()
-    {
-        int randomIndex = Random.Range(0, engineSounds.Length - 1);
-        audioSource.clip = engineSounds[randomIndex];
-        audioSource.Play();
+        public IEnumerator MoveToADestination(Vector3 destination)
+        {
+            PlayRandomEngineSound();
+            while ((destination - transform.position).magnitude > 0.01f)
+            {
+                transform.position = Vector3.Lerp(transform.position, destination, moveSpeed * Time.deltaTime);
+                yield return null;
+            }
+
+            transform.position = destination; //Snap position destination position
+            audioSource.Stop();
+        }
+
+        protected void PlayRandomEngineSound()
+        {
+            int randomIndex = Random.Range(0, engineSounds.Length - 1);
+            audioSource.clip = engineSounds[randomIndex];
+            audioSource.Play();
+        }
     }
 }
