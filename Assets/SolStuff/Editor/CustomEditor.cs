0749fcd1426bc45f93dbf95169b346cf70c7ac0d
Silly Sol <nemo637revlis@gmail.com>
2025-07-24 19:20:46 +1000

    created decoy item

---
+++ Assets/SolStuff/Editor/CustomEditor.cs
@@ -0,0 +1,28 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(DecoyItem), true)]
+public class SolDecoyEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Interact"))
+        {
+            // ‘target’ is the magic variable that editors use to link back to the original component. It’s in the BASE CLASS, so you have to ‘cast’ to get access to YOUR functions.
+            DecoyItem decoyItem; 
+            decoyItem = target as DecoyItem;
+            decoyItem?.Interact();
+        }
+        
+        if (GUILayout.Button("Stop Interacting"))
+        {
+            // ‘target’ is the magic variable that editors use to link back to the original component. It’s in the BASE CLASS, so you have to ‘cast’ to get access to YOUR functions.
+            DecoyItem decoyItem; 
+            decoyItem = target as DecoyItem;
+            decoyItem?.StopInteracting();
+        }
+    }
+}
+
