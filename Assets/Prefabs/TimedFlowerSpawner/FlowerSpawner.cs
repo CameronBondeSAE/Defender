f721f17ee7b8cc90d1667925080f6204a1a4ca1a
Cameron Bonde <c.bonde@sae.edu>
2025-07-29 02:59:13 +1000

    Faster player

--- Assets/Prefabs/TimedFlowerSpawner/FlowerSpawner.cs
+++ Assets/Prefabs/TimedFlowerSpawner/FlowerSpawner.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class FlowerSpawner : MonoBehaviour, IUseableObject
+public class FlowerSpawner : MonoBehaviour//, IUseableObject
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
