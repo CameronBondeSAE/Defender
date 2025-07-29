ec819fbe7f9598902617f58f66953f7062c938a7
upandadz <1033963@student.sae.edu.au>
2025-07-29 14:03:46 +1000

    Added materials & began MindControlGrenade
    Just added the visual for the mind control grenade and the script with some pseudocode

---
+++ Assets/Prefabs/Items/MindControlGrenade/MindControlGrenade.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace adamsScripts
+{
+    public class MindControlGrenade : MonoBehaviour
+    {
+        // inherits from base grenade/item script? - would need to be picked up, activated/thrown/rolled
+        // after launched will need to explode, have explosion radius, change ai of aliens hit - maybe even civilians?, play sound, spawn particles
+    }
+}
