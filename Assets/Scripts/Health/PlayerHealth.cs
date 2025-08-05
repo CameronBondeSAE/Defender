f4c6221f1fe574b96fa18ced5cee1f40989884c1
Cameron Bonde <c.bonde@sae.edu>
2025-08-05 16:48:48 +1000

    Food assets. Death revive on player

--- Assets/Scripts/Health/PlayerHealth.cs
+++ Assets/Scripts/Health/PlayerHealth.cs
@@ -16,9 +16,11 @@ public class PlayerHealth : Health
     private IEnumerator RespawnPlayer()
     {
         yield return new WaitForSeconds(deathAnimDuration);
+        yield return new WaitForSeconds(reviveAnimDuration);
         currentHealth = maxHealth;
-        isDead = false;
         base.Revive();
+        
+
         //PlayerEventManager.instance.events.onIdle.Invoke();
         transform.position = new Vector3(0, 1, 0); // change to spawn position when that's been decided
     }
