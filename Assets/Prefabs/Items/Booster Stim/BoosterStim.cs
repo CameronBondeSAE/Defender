46594ed1cc26b690f0f0fe61a8b7b22bb4dd9632
N1ck5tar <1032458@student.sae.edu.au>
2025-08-05 17:30:16 +1000

    work in progress, quick test

--- Assets/Prefabs/Items/Booster Stim/BoosterStim.cs
+++ Assets/Prefabs/Items/Booster Stim/BoosterStim.cs
@@ -1,7 +1,10 @@
+using System.Collections;
 using UnityEngine;
 
 public class BoosterStim : MonoBehaviour, IUsable
 {
+    private float stimDuration = 10f;
+
     public void StopUsing()
     {
         Debug.Log("Booster Stim, StopUsing");
@@ -15,14 +18,25 @@ public class BoosterStim : MonoBehaviour, IUsable
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    
+    public void ActivateBoosterStim()
+    {
+        StartCoroutine(speedBoostTimer());
+
+    }
+
+    IEnumerator speedBoostTimer()
+    {
+        // speed player
+        yield return new WaitForSeconds(stimDuration);
+
+    }
 }
