2158f4080aa5e492b181ce8b1b95fa33f141acf3
N1ck5tar <1032458@student.sae.edu.au>
2025-08-05 17:02:02 +1000

    added debug log to the IUsable (:

--- Assets/Prefabs/Items/Booster Stim/BoosterStim.cs
+++ Assets/Prefabs/Items/Booster Stim/BoosterStim.cs
@@ -4,12 +4,12 @@ public class BoosterStim : MonoBehaviour, IUsable
 {
     public void StopUsing()
     {
-        
+        Debug.Log("Booster Stim, StopUsing");
     }
 
     public void Use()
     {
-        
+        Debug.Log("Booster Stim, Use");
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
