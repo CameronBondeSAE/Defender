2b923db67af82bc0993108a4b13828d69ed4c0c8
N1ck5tar <1032458@student.sae.edu.au>
2025-08-06 17:25:31 +1000

    added black material in material folder, created asset for stim booster, stim now runs out + a visual indicator

--- Assets/Prefabs/Items/Booster Stim/BoosterStim.cs
+++ Assets/Prefabs/Items/Booster Stim/BoosterStim.cs
@@ -5,46 +5,59 @@ public class BoosterStim : MonoBehaviour, IUsable
 {
     [SerializeField] private float stimDuration = 10f;
     [SerializeField] private float speedIncrease = 4f;
+    [SerializeField] private bool stimUsed = false;
 
     [SerializeField] private PlayerMovement playerMovement;
 
-    public void StopUsing()
+    [SerializeField] private GameObject stimBoostVisual;
+
+    public void StopUsing() // IUsable
     {
         Debug.Log("Booster Stim, StopUsing");
     }
 
-    public void Use()
+    public void Use() // IUsable
     {
         Debug.Log("Booster Stim, Use");
-       //playerMovement = GetComponentInParent<PlayerMovement>(); // NOTE - when items get put as a child use this instead
+       //playerMovement = GetComponentInParent<PlayerMovement>(); // NOTE - when items get put as a child use this instead?
         playerMovement = FindAnyObjectByType<PlayerMovement>(); // temporary
-        ActivateBoosterStim();
+
+        if (stimUsed == false)
+        {
+            ActivateBoosterStim();
+        }
+
+
+        if(stimBoostVisual != null)
+        {
+            Renderer render = stimBoostVisual.GetComponent<Renderer>();
+            render.material.color = Color.black;
+        }
 
     }
 
-    public void ActivateBoosterStim()
+    public void ActivateBoosterStim() // start
     {
+        stimUsed = true;
         playerMovement.MoveSpeed += speedIncrease; 
         StartCoroutine(BoosterStimDuration());
 
     }
 
-    IEnumerator BoosterStimDuration()
+    IEnumerator BoosterStimDuration() // while
     {
-        Debug.Log("stim timer active");
         yield return new WaitForSeconds(stimDuration);
 
         DeactivateBoosterStim();
     }
 
-    public void DeactivateBoosterStim()
+    public void DeactivateBoosterStim() // end
     {
         playerMovement.MoveSpeed -= speedIncrease;
     }
 
     // TO-DO LIST / IDEAS
 
-    // Rechargeable stim??
     // stim affects everyone
     // network it
 
