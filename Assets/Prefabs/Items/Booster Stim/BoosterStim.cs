491824ce481ce745e45da700af745fc057d975b5
N1ck5tar <1032458@student.sae.edu.au>
2025-08-08 18:15:12 +1000

    implemented IPickup on boost stim, added rigidbody,

--- Assets/Prefabs/Items/Booster Stim/BoosterStim.cs
+++ Assets/Prefabs/Items/Booster Stim/BoosterStim.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using UnityEngine;
 
-public class BoosterStim : MonoBehaviour, IUsable
+public class BoosterStim : MonoBehaviour, IUsable, IPickup
 {
     [SerializeField] private float stimDuration = 10f;
     [SerializeField] private float speedIncrease = 4f;
@@ -11,12 +11,15 @@ public class BoosterStim : MonoBehaviour, IUsable
 
     [SerializeField] private GameObject stimBoostVisual;
 
-    public void StopUsing() // IUsable
+
+    #region IUsable
+
+    public void StopUsing()
     {
-        Debug.Log("Booster Stim, StopUsing");
+        //Debug.Log("Booster Stim, StopUsing");
     }
 
-    public void Use() // IUsable
+    public void Use()
     {
         Debug.Log("Booster Stim, Use");
        //playerMovement = GetComponentInParent<PlayerMovement>(); // NOTE - when items get put as a child use this instead?
@@ -36,12 +39,30 @@ public class BoosterStim : MonoBehaviour, IUsable
 
     }
 
+    #endregion
+
+    #region IPickup
+
+    public void Pickup()
+    {
+        //Debug.Log("Stim PickedUp");
+    }
+
+    public void Drop()
+    {
+        //Debug.Log("Stim Dropped");
+    }
+
+    #endregion
+
+
     public void ActivateBoosterStim() // start
     {
         stimUsed = true;
+        //Debug.Log("Stim Boost Active");
+
         playerMovement.MoveSpeed += speedIncrease; 
         StartCoroutine(BoosterStimDuration());
-
     }
 
     IEnumerator BoosterStimDuration() // while
@@ -54,12 +75,13 @@ public class BoosterStim : MonoBehaviour, IUsable
     public void DeactivateBoosterStim() // end
     {
         playerMovement.MoveSpeed -= speedIncrease;
+        //Debug.Log("Stim Boost Deactive");
     }
 
-    // TO-DO LIST / IDEAS
 
-    // stim affects everyone
-    // network it
+    // TO-DO LIST / IDEAS (nickA)
+
+    // network it (sync for everyone + position and use), sync stimUsed variable for late join?
 
 
 }
