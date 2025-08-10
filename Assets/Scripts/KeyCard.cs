f38ac817f385d618dbcad7a16578eec452a0dfdc
Jai Vanderark <1027859@student.sae.edu.au>
2025-08-10 15:33:51 +1000

    base keycard - door working

--- Assets/Scripts/KeyCard.cs
+++ Assets/Scripts/KeyCard.cs
@@ -1,16 +1,59 @@
+using System;
 using UnityEngine;
 
-public class KeyCard : MonoBehaviour
+public class KeyCard : UsableItem_Base
 {
+    [Header("Keycard Settings")]
+    public string KeyId;
+    public Material keycardUsedMat;
+    public float useTime;
+    public GameObject currentGate;
+
+    public bool inUse;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    
+    protected override void Awake()
     {
-        
+        base.Awake();
+        activationCountdown = useTime;
+    }
+    public override void Use()
+    {
+        if (currentGate != null && !inUse)
+        {
+            base.Use();
+            inUse = true;
+            Debug.Log("KeyCard used");
+            GetComponentInChildren<MeshRenderer>().material =  keycardUsedMat;
+		
+            if (activationCountdown > 0)
+                StartActivationCountdown_LocalUI(Mathf.CeilToInt(activationCountdown));
+        }
+    }
+    
+    protected override void ActivateItem()
+    {
+        Debug.Log("DemoItem ACTIVATED!");
+        GetComponentInChildren<MeshRenderer>().material.color = Color.yellow;
+        currentGate.GetComponent<SlidingDoor>().isOpen = true;
+        Destroy(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.GetComponent<SlidingDoor>() != null)
+        {
+            currentGate = other.gameObject;
+        }
+    }
+
+    public override void Pickup()
+    {
+        base.Pickup(); // Plays pickup sound, etc
+    }
+
+    public override void Drop()
+    {
+        base.Drop(); // Plays drop sound, etc
     }
 }
