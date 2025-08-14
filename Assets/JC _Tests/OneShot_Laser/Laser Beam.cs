232a2a7522c1311eea7aa85974d684ab8b65d412
Jazz-Owlz <167410150+Jazz-Owlz@users.noreply.github.com>
2025-08-14 14:42:35 +1000

    update

--- Assets/JC _Tests/OneShot_Laser/Laser Beam.cs
+++ Assets/JC _Tests/OneShot_Laser/Laser Beam.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class LaserBeam : MonoBehaviour
 {
+   // weapon damage - reference beam script
+       public int LaserDamage = 5;
+       public int charge1Damage = 10;
+       public int charge2Damage = 20;
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
--- Assets/JC _Tests/OneShot_Laser/Laser Gun Script.cs
+++ Assets/JC _Tests/OneShot_Laser/Laser Gun Script.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,43 +11,58 @@ public class Laser_Gun_Script : MonoBehaviour, IUsable, IPickup
     public GameObject AimBeam;
     public bool isGrabbed = false; // detects if a weapon has been picked up 
     public bool gunFired = false; // this checks for when the gun on hand has been fired 
-    public bool Charging = false;
-    
-    // weapon damage - reference beam script
-    public int LaserDamage = 5;
-    public int charge1Damage = 10;
-    public int charge2Damage = 20;
-    
-   
-    
+    public bool isCharging = false;
+
+    public LaserBeam laserBeam;
+
     // Weapon charge
-    public float maxCharge = 5.0f;
+    public float maxCharge = 7.0f;
     public float chargeTime = 0f;
+
     public void ShotsFired()
     {
-        
+        // else if (InputSystem.GetDevice<Mouse>().leftButton.isPressed)
+        // {
+        //     isCharging = true;
+        // }
     }
 
     public void ShotCharge()
     {
-        if (!Charging)
+        if (!isCharging) // cold resolve an issue uising the bool here
         {
-            chargeTime += Time.deltaTime;
-            /// how do you say the charge damage do you jst make some if statements
+            StartCoroutine(ChargingShot());
         }
     }
+
+    IEnumerator ChargingShot()
+    {
+        if (chargeTime <= 4.0f) // fires single shot dealing 5 damage //
+        {
+            Instantiate(LBeam, transform.position, transform.rotation);
+        }
+
+        if (chargeTime >= 4f) // 10 damage
+        {
+        }
+
+        if (chargeTime == maxCharge) // 15 damage
+        {
+            //
+        }
+    }
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         LBeam.SetActive(false);
-        AimBeam.SetActive(true);
+        AimBeam.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void Use() // laser gun is aquired and is held by the player unil its fired (used)
@@ -55,10 +72,10 @@ public class Laser_Gun_Script : MonoBehaviour, IUsable, IPickup
 
     public void StopUsing()
     {
-        ShotsFired()
+        // if
+        //ShotsFired()
     }
-    
-    
+
 
     public void Pickup()
     {
