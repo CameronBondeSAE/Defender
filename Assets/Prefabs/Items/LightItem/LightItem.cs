5cfc8204212bab695593ef3a8c0c1a1f6a40e409
steven <1035350@student.sae.edu.au>
2025-11-26 17:40:35 +1100

    item level set up for coding item

--- Assets/Prefabs/Items/LightItem/LightItem.cs
+++ Assets/Prefabs/Items/LightItem/LightItem.cs
@@ -1,17 +1,32 @@
+using System.Collections;
 using Defender;
 using UnityEngine;
 
-public class LightItem : MonoBehaviour, IUsable
+public class LightItem : MonoBehaviour, IUsable, IPowerable
 {
+    public Light spotLight;
+    private IEnumerator timeCoroutine;
     public void Use(CharacterBase characterTryingToUse)
     {
-   
+        
         Debug.Log("Trying to use light item "+characterTryingToUse.name);
+        
+        spotLight.enabled = !spotLight.enabled;
+        
+    }
+    // write down and make the light a toggle, as well as implement a timer for the light to turn off after some time
+
+    
+    public void StopUsing()
+    {
+        
+        Debug.Log("Stopping light");
         
     }
 
-    public void StopUsing()
+    public void SetPowered(bool powered)
     {
-        Debug.Log("Stopping light");
+        
     }
+    
 }
