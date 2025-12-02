e632d0e1797736212f1469bc264c23bb4828f746
Ashley Pearson <ashleypea@gmail.com>
2025-12-02 17:42:51 +1000

    Work on Lobby Prefab and script set up. TO DO: Integrate / test with existing network manager. TO DO: UI Transitions.

---
+++ Assets/Team members work space/AshleyPearson/Scripts/LobbyEntry.cs
@@ -0,0 +1,34 @@
+using System;
+using TMPro;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AshleyPearson
+{
+    public class LobbyEntry : MonoBehaviour
+    {
+        [SerializeField] private Text lobbyNameText;
+        [SerializeField] private Text playerCountText;
+        [SerializeField] private Button joinLobbyButton;
+
+        private string joinCode;
+
+        public void SetLobbyEntryInfo(string lobbyName, int playerCount, string joinCode)
+        {
+            //Set lobby entry information for UI from network manager / relay manager
+            lobbyNameText.text = lobbyName;
+            playerCountText.text = playerCount.ToString();
+            this.joinCode = joinCode;
+            
+            Debug.Log("LobbyEntry: Lobby Name " +  lobbyName +  " - JoinCode is " + joinCode);
+        }
+
+        public void OnButtonClicked_JoinLobby()
+        {
+            //Use static event to join relevant session
+            LobbyEvents.OnButtonClicked_JoinLobby?.Invoke(this.joinCode);
+        }
+
+    }
+}
---
+++ Assets/Team members work space/AshleyPearson/Scripts/LobbyEvents.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+namespace AshleyPearson
+{
+    public class LobbyEvents : MonoBehaviour
+    {
+        public static Action<string> OnButtonClicked_JoinLobby;
+    }
+}
