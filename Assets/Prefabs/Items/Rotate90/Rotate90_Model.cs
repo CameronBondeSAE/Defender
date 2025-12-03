0397de45bd0e1611e4cd425c16bdc9177f55c29e
Milo2133 <1030776@student.sae.edu.au>
2025-12-03 11:11:09 +1100

    Added a particle system to the rotate 90 wall as well as a adding a material to it

--- Assets/Prefabs/Items/Rotate90/Rotate90_Model.cs
+++ Assets/Prefabs/Items/Rotate90/Rotate90_Model.cs
@@ -12,6 +12,8 @@ public class Rotate90_Model : NetworkBehaviour, IUsable
 	    Debug.Log(name + " : "+ characterTryingToUse.name + " is using");
 	    transform.DORotate(new Vector3(0,transform.eulerAngles.y+90f,0), 2f, RotateMode.Fast);
 	    view.stoneSound_RPC();
+	    view.stoneEmitRight_RPC();
+	    view.stoneEmitLeft_RPC();
     }
 
     public void StopUsing()
