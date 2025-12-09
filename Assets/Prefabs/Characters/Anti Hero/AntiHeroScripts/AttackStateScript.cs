53fe8e404e7e4ba581dd5542030685597c6a9f0a
Mirza Wilayat Ali <mirza.wilayat.ali148@gmail.com>
2025-12-09 14:17:06 +1100

    Anti Hero can now kill Aliens successfully

--- Assets/Prefabs/Characters/Anti Hero/AntiHeroScripts/AttackStateScript.cs
+++ Assets/Prefabs/Characters/Anti Hero/AntiHeroScripts/AttackStateScript.cs
@@ -3,26 +3,39 @@ using UnityEngine;
 
 public class AttackStateScript : AntAIState
 {
-    private GameObject targetAlien;
-    private LaserEyes laserEyes;
-    private VisionSystem visionSystem;
+    public LaserEyes laserEyes;
+    private AntiHeroAISense antiHeroAISense;
+    private Animator animator;
     public override void Create(GameObject aGameObject)
     {
-        visionSystem =  aGameObject.GetComponent<VisionSystem>();
         laserEyes = aGameObject.GetComponent<LaserEyes>();
+        antiHeroAISense = aGameObject.GetComponent<AntiHeroAISense>();
+        animator = aGameObject.GetComponent<Animator>();
     }
     public override void Enter()
     {
-       
+       laserEyes.enabled = true;
+       laserEyes.targetAlien = antiHeroAISense.latestSeenAlien;
+       animator.Play("Attack");
     }
 
     public override void Execute(float aDeltaTime, float aTimeScale)
     {
-        
+        if (laserEyes.targetAlien != null)
+        {
+            laserEyes.targetAlien.GetComponent<Health>().TakeDamage(1);
+            if (laserEyes.targetAlien.GetComponent<Health>().isDead)
+            {
+                laserEyes.DisableBeams();
+                Finish();
+            }
+        }
     }
 
     public override void Exit()
     {
-        
+        laserEyes.targetAlien = null;
+        laserEyes.DisableBeams();
+        laserEyes.enabled = false;
     }
 }
