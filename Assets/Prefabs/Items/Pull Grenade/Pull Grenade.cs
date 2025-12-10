e035a790a814a951960aa4d2ebfb6ec5b195d6a5
kennithhixnt <kennethbegg0@gmail.com>
2025-12-10 19:05:26 +1100

    created a pull grenade

--- Assets/Prefabs/Items/Pull Grenade/Pull Grenade.cs
+++ Assets/Prefabs/Items/Pull Grenade/Pull Grenade.cs
@@ -1,14 +1,13 @@
+using UnityEngine;
 using Defender;
 using Unity.Netcode;
-using UnityEngine;
 
-public class PushGrenade : UsableItem_Base
+public class PullGrenade : UsableItem_Base
 {
     [Header("Grenade Stats")]
     [SerializeField] private float explosionRadius = 5f;
-    [SerializeField] private float explosionForce = 10f;
+    [SerializeField] private float pullForce = 10f;
     [SerializeField] private float grenadeCountdown = 3f;
-    
 
     protected override void Awake()
     {
@@ -20,12 +19,12 @@ public class PushGrenade : UsableItem_Base
     public override void Pickup(CharacterBase whoIsPickupMeUp)
     {
         base.Pickup(whoIsPickupMeUp);
-        Debug.Log(" picked up");
+        Debug.Log("picked up");
     }
 
     public override void Use(CharacterBase characterTryingToUse)
     {
-        Debug.Log($"[GrenadePushAll.Use] Used by {characterTryingToUse?.name}");
+        Debug.Log($"[PullGrenade.Use] Used by {characterTryingToUse?.name}");
 
         if (IsServer && characterTryingToUse != null)
         {
@@ -65,35 +64,36 @@ public class PushGrenade : UsableItem_Base
 
     private void ExplodeServer()
     {
-        Debug.Log("[ExplodeServer] Grenade exploding");
+        Debug.Log("[ExplodeServer] Pull grenade exploding ");
 
         Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+
         foreach (var col in colliders)
         {
-            Vector3 direction = (col.transform.position - transform.position).normalized;
+            // Direction **toward** the grenade
+            Vector3 direction = (transform.position - col.transform.position).normalized;
             float distance = Vector3.Distance(transform.position, col.transform.position);
             float forceMultiplier = 1 - (distance / explosionRadius); // falloff
 
-          
-            // Rigidbody push
+            // Rigidbody pull
             Rigidbody rb = col.GetComponent<Rigidbody>();
-            if (rb != null)
+            if (rb != null && !rb.isKinematic)
             {
-                rb.AddExplosionForce(explosionForce * forceMultiplier, transform.position, explosionRadius, 1f, ForceMode.Impulse);
+                rb.AddForce(direction * pullForce * forceMultiplier, ForceMode.Impulse);
             }
             else
             {
-                // CharacterController push
+                // CharacterController pull
                 var cc = col.GetComponent<CharacterController>();
                 if (cc != null)
                 {
-                    cc.Move(direction * explosionForce * forceMultiplier * Time.fixedDeltaTime);
+                    // Remove Time.fixedDeltaTime to make movement noticeable
+                    cc.Move(direction * pullForce * forceMultiplier);
                 }
             }
         }
-        
+
         DestroyItem_Server();
     }
 }
-
 
