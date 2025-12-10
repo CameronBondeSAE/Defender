97b4673154b96e7403124d29c02d235ef1cc8dd6
Steven Harvie <1035350@student.sae.edu.au>
2025-12-10 17:42:15 +1100

    Radar item testing

--- Assets/Prefabs/Items/Radar/RadarRays.cs
+++ Assets/Prefabs/Items/Radar/RadarRays.cs
@@ -4,30 +4,39 @@ using UnityEngine;
 
 public class RadarRays : UsableItem_Base
 {
-	public float speed     = 10f;
-	public bool activated;
+    public float speed     = 5f;
+    public bool  activated = false;
 
-	public override void Use(CharacterBase characterTryingToUse)
-	{
-		base.Use(characterTryingToUse);
+    public override void Use(CharacterBase characterTryingToUse)
+    {
+        base.Use(characterTryingToUse);
 
-		activated = !activated;
-	}
-	public override void Pickup(CharacterBase whoIsPickupMeUp)
-	{
-		base.Pickup(whoIsPickupMeUp);
+        activated = !activated;
+    }
+    public override void Pickup(CharacterBase whoIsPickupMeUp)
+    {
+        base.Pickup(whoIsPickupMeUp);
 		
-		Debug.Log(whoIsPickupMeUp.name);
-	}
-	void Update()
-	{
-		if (activated)
-		{
-			transform.Rotate(0, speed, 0);
-			
-			RaycastHit hit;
-			Physics.Raycast(transform.position, transform.forward, out hit, 99999f);
-			Debug.DrawRay(transform.position, transform.forward * hit.distance, Color.red);
-		}
-	}
+        Debug.Log("Tornado Item picked up by : "+whoIsPickupMeUp.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activated)
+        {
+            transform.Rotate(0, speed, 0);
+            
+            RaycastHit hit;
+            Physics.Raycast(transform.position, transform.forward, out hit, 99999f);
+            Debug.DrawRay(transform.position, transform.forward * hit.distance, Color.red);
+            
+            GetComponent<LineRenderer>().SetPosition(1, hit.point);
+
+            if (CompareTag("Alien")) // tag == "Alien"
+            {
+                
+            }
+        }
+    }
 }
