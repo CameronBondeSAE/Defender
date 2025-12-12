19b53c7cc5c9bceda7a750f6a989145d27e6c961
Divij2709 <divij.seedat@gmail.com>
2025-12-13 09:43:46 +1100

    SpeedUpZone Functionality
    Speed Up Zone works in Test Scene When placing player object in it, doubles move speed for 15 seconds

--- Assets/Prefabs/Items/Speed Zone/SpeedZone.cs
+++ Assets/Prefabs/Items/Speed Zone/SpeedZone.cs
@@ -1,16 +1,55 @@
 using UnityEngine;
+using Defender;
 
 public class SpeedZone : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Speed Zone Seettings")] 
+    
+    public float boostMultiplier = 2f;
+    public float boostDuration = 15f;
+
+    private CharacterBase character;
+    private float defaultSpeed;
+    private float boostEndTime;
+    private bool isBoosting = false;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        character = other.GetComponentInParent < CharacterBase>();
+
+        if (character != null)
+        {
+            StartBoost();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (isBoosting && Time.time >= boostEndTime)
+        {
+            ResetSpeed();
+        }
     }
+
+    private void StartBoost()
+    {
+        if (character == null) return;
+
+        defaultSpeed = character.DefaultSpeed;
+
+        character.MoveSpeed = defaultSpeed * boostMultiplier;
+
+        boostEndTime = Time.time + boostDuration;
+
+        isBoosting = true;
+    }
+
+    private void ResetSpeed()
+    {
+        if (character == null) return;
+
+        character.MoveSpeed = defaultSpeed;
+        isBoosting = false;
+    }
+
 }
